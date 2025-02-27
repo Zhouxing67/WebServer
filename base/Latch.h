71a635026f58c31403bd30a94655d667d933bba1
zhouX <zhouxing_se@qq.com>
2025-02-27 21:44:23 +0800

    实现Latch

---
+++ base/Latch.h
@@ -0,0 +1,32 @@
+#ifndef LATCH_H
+#define LATCH_H
+#include "copyable.h"
+#include <condition_variable>
+#include <mutex>
+
+using std::condition_variable;
+using std::mutex;
+using std::unique_lock;
+
+class Latch: public noncopyable_and_nonmoveable
+{
+  public:
+    Latch(int count) : count_(count) {}
+    void wait()
+    {
+        std::unique_lock<mutex> lck;
+        cv_.wait(lck, [this] { return count_ <= 0; });
+    }
+    void signal()
+    {
+        count_--;
+        if (count_ <= 0)
+            cv_.notify_all();
+    }
+
+  private:
+    int count_;
+    condition_variable cv_;
+    mutex mut_;
+};
+#endif
