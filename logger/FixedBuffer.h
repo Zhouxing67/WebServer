278edeebadd6a718edeec30cdcad54b59afea0e3
zhouX <zhouxing_se@qq.com>
2025-02-27 21:32:32 +0800

    修改FixedBuffer的容量为可设置

--- logger/FixedBuffer.h
+++ logger/FixedBuffer.h
@@ -5,12 +5,14 @@
 #include <strings.h>
 using std::string;
 using std::string_view;
+
 static const int KFIXEDBUFFERSIZE = 4096;
+static const int KFIXEDLARGERBUFFERSIZE = 8192;
 
 class FixedBuffer
 {
   public:
-    FixedBuffer() : data_(new char[KFIXEDBUFFERSIZE]), cur_(data_){};
+    FixedBuffer(int capacity = KFIXEDBUFFERSIZE) : capacity_(capacity), data_(new char[capacity_]), cur_(data_){};
     ~FixedBuffer()
     {
         delete[] data_;
@@ -24,11 +26,12 @@ class FixedBuffer
     const char *data() const { return data_; }
     int size() const { return static_cast<int>(cur_ - data_); }
 
-    int avail() const { return data_ + KFIXEDBUFFERSIZE - cur_; }
+    int avail() const { return data_ + capacity_ - cur_; }
     void reset() { cur_ = data_; }
-    void clear() { bzero(data_, KFIXEDBUFFERSIZE); }
+    void clear() { bzero(data_, capacity_); }
 
   private:
+    const int capacity_;
     char *data_;
     char *cur_ = nullptr;
 };
